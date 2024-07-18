d44a3f7defeea2c1670c8e8c7a113880cdcbe2a2
mehdi <mojtahedwork@gmail.com>
2024-07-18 21:30:00 +0330

    init

---
+++ App.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+
+const app = express()
+od
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/get-profile', (req, res) => {
+    let response = res;
+    MongoClient.connect('mongodb://admin:password@localhost:27017', (e, c) => {
+        if (e) throw e;
+
+        let db = c.db('user-account')
+        let query = {userid: 1}
+        db.collection('users').findOne(query, (e, r) => {
+            if (e) throw e;
+            c.close();
+            response.send(r)
+        })
+    })
+
+})
