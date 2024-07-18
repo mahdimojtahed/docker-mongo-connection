549a96b15338cccdd34b254773fd32ac9f6573d6
mehdi <mojtahedwork@gmail.com>
2024-07-18 22:24:12 +0330

    added compose

--- App.js
+++ App.js
@@ -34,6 +34,6 @@ app.get('/get-profile', async (req, res) => {
     }
 });
 
-app.listen((3000), () => {
-    console.log('Server is running on port 3000')
+app.listen((4000), () => {
+    console.log('Server is running on port 4000')
 });
